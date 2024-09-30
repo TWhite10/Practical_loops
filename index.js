c558f28f9fd3031e4e2db40af4de8c770dd70e95
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-09-30 14:41:15 -0400

    Fizz Buzz

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+
+//Loop through all number from 1 to 100
 n = 0
 while (n <= 99){
     n++
     console.log(n)
 }
 
+//Fizz Buzz 
+let n = 1
+while (n <= 100){
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log(n + ` Fizz Buzz`)
+    }
+    else if (n % 3 === 0){
+       console.log ( n + ` Fizz`) 
+    }
+    else if (n % 5 === 0){
+        console.log(n + ` Buzz`)
+    }
+    n++;
+}
+  
